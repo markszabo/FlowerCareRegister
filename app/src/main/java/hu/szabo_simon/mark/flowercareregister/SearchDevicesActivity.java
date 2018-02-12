19fe3beccaf1cb321c1212772d41d519c63fad1a
markszabo <markszabo@users.noreply.github.com>
2018-02-12 17:01:02 +0100

    Search works and nice but back button is missing

--- app/src/main/java/hu/szabo_simon/mark/flowercareregister/SearchDevicesActivity.java
+++ app/src/main/java/hu/szabo_simon/mark/flowercareregister/SearchDevicesActivity.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,7 +21,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 import java.util.ArrayList;
 
-public class SearchDevicesActivity extends Activity {
+public class SearchDevicesActivity extends AppCompatActivity {
     private LeDeviceListAdapter mLeDeviceListAdapter;
     private BluetoothAdapter mBluetoothAdapter;
     private boolean mScanning;
