221f59852f5d5e401ebe724e6a02b7907fc2c696
markszabo <markszabo@users.noreply.github.com>
2018-02-17 20:27:42 +0100

    Release 1.0

--- app/src/main/java/hu/szabo_simon/mark/flowercareregister/MainActivity.java
+++ app/src/main/java/hu/szabo_simon/mark/flowercareregister/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.findViewById(android.R.id.content).setKeepScreenOn(true); //prevent from going to sleep while app is running
+        this.findViewById(android.R.id.content).setKeepScreenOn(true); // prevent from going to sleep while app is running
 
         displayProgressBarAndRegisterButton(false);
 
